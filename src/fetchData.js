7f4954ea5f61c20f9b7dc7bb5f90301b54572849
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-04-09 01:15:21 +0300

    Add

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,7 +1,39 @@
-const test = document.querySelector('#test')
+// import defaultDisplay from './index';
+import Snow from './img/snow.jpg';
+import Sun from './img/sun.jpg';
+import Rain from './img/rain.jpg';
+import Clouds from './img/clouds.jpg';
+import Else from './img/else.jpg';
 
-async function getData(){
-  const fet = await fetch('http://api.openweathermap.org/data/2.5/weather?q=istanbul&appid=87661441a138699d2c9a259ebfb1c52f');
-  const res = await fet.json();
-  console.log(res)
+
+export  const dataObj = {}
+
+export async function getData(city) {
+  const fet = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=87661441a138699d2c9a259ebfb1c52f`);
+  const data = await fet.json();
+    dataObj['name'] = data.name;
+    dataObj['temp'] = data.main.temp;
+    dataObj['feels'] = data.main.feels_like;
+    dataObj['humidity'] = data.main.humidity;
+    dataObj['weatherDes'] = data.weather[0].main;
+    console.log(dataObj);
+    
+}
+
+
+export function changeBackGrounf(img){
+  if (dataObj['weatherDes'] === 'Snow') {
+    img.src = Snow;
+  } else if (dataObj['weatherDes'] === 'Clouds'){
+    img.src = Clouds;
+  }
+  else if (dataObj['weatherDes'] === 'Clear') {
+    img.src = Sun;
+  }
+  else if (dataObj['weatherDes'] === 'Rain') {
+    img.src = Rain;
+  }
+  else  {
+    img.src = Else;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,49 @@
-const test = document.querySelector('#test');
+import { getData, dataObj, changeBackGrounf} from './fetchData';
+import Else from './img/else.jpg';
+
 
 const searchBtn = document.querySelector('#searchBtn');
 const weatherImg = document.querySelector('#weather-img');
+const clsBtn = document.querySelector('#cles-btn');
+const cityName = document.querySelector('#city-name');
+const tempDisplay = document.querySelector('#temp-degree');
+const feelsLike = document.querySelector('#feel-like');
+const humidityDisplay = document.querySelector('#humidity');
+const weatherCond = document.querySelector('#weather-condition');
+const table = document.querySelector('table');
 
-async function getData(city) {
-  const fet = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=87661441a138699d2c9a259ebfb1c52f`);
-  const data = await fet.json();
-   test.innerHTML = data.main.temp;
-  if (data.main.temp - 273 > 10) {
-     weatherImg.src = 'https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80'
-   } 
-   else {
-     weatherImg.src = 'https://images.unsplash.com/photo-1512511708753-3150cd2ec8ee?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80'
-   }
-}
+
+weatherImg.src = Else;
+table.classList.add('hide')
+
+
 
 searchBtn.addEventListener('click', (e)=>{
   e.preventDefault();
   const search = document.querySelector('#search').value;
-  getData(search);
-} )
+  getData(search).then(()=>{
+    cityName.classList.add('text-primary')
+    cityName.classList.remove('text-danger');
+    weatherCond.classList.remove('hide');
+    weatherCond.classList.add('show');
+    table.classList.remove('hide');
+    table.classList.add('show');
+    weatherCond.innerHTML = `Weather Condition: ${dataObj['weatherDes']}`;
+    cityName.innerHTML =  `City Name: ${dataObj['name']}`;
+    tempDisplay.innerHTML = dataObj['temp'];
+    feelsLike.innerHTML = dataObj['feels'];
+    humidityDisplay.innerHTML = dataObj['humidity'];
+    changeBackGrounf(weatherImg)
+  }).catch(()=>{
+    cityName.innerHTML = 'Wrong City Name !';
+    cityName.classList.remove('text-primary')
+    cityName.classList.add('text-danger');
+    weatherImg.src = Else;
+    weatherCond.classList.add('hide');
+    weatherCond.classList.remove('show');
+    table.classList.add('hide');
+    table.classList.remove('show');
+  })
+})
+
+// clsBtn.addEventListener('click' ,  )

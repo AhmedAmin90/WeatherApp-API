9fb26cbe59ef96d5f5e03cda65fcba7b80fd863d
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-04-09 01:31:20 +0300

    Add main features

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import { getData, dataObj, changeBackGrounf} from './fetchData';
 import Else from './img/else.jpg';
 
 
+// Main elements:
 const searchBtn = document.querySelector('#searchBtn');
 const weatherImg = document.querySelector('#weather-img');
-const clsBtn = document.querySelector('#cles-btn');
+const changeBtn = document.querySelector('#changeBtn');
 const cityName = document.querySelector('#city-name');
 const tempDisplay = document.querySelector('#temp-degree');
 const feelsLike = document.querySelector('#feel-like');
@@ -12,22 +13,28 @@ const humidityDisplay = document.querySelector('#humidity');
 const weatherCond = document.querySelector('#weather-condition');
 const table = document.querySelector('table');
 
-
+// Main style and content:
 weatherImg.src = Else;
-table.classList.add('hide')
+table.classList.add('hide');
+changeBtn.classList.add('hideVis')
 
 
-
+// Evenets:
 searchBtn.addEventListener('click', (e)=>{
   e.preventDefault();
   const search = document.querySelector('#search').value;
   getData(search).then(()=>{
+    // Setup the elements:
     cityName.classList.add('text-primary')
     cityName.classList.remove('text-danger');
     weatherCond.classList.remove('hide');
     weatherCond.classList.add('show');
     table.classList.remove('hide');
     table.classList.add('show');
+    changeBtn.classList.add('show');
+    changeBtn.classList.remove('hideVis');
+
+    // Add content:
     weatherCond.innerHTML = `Weather Condition: ${dataObj['weatherDes']}`;
     cityName.innerHTML =  `City Name: ${dataObj['name']}`;
     tempDisplay.innerHTML = dataObj['temp'];
@@ -35,14 +42,15 @@ searchBtn.addEventListener('click', (e)=>{
     humidityDisplay.innerHTML = dataObj['humidity'];
     changeBackGrounf(weatherImg)
   }).catch(()=>{
+    weatherImg.src = Else;
     cityName.innerHTML = 'Wrong City Name !';
     cityName.classList.remove('text-primary')
     cityName.classList.add('text-danger');
-    weatherImg.src = Else;
     weatherCond.classList.add('hide');
     weatherCond.classList.remove('show');
     table.classList.add('hide');
     table.classList.remove('show');
+    changeBtn.classList.add('hideVis');
   })
 })
 
